58e08b071369a1b203e0a713a2920858fca70339
zguiyang <zhaoguiyang18@outlook.com>
2026-02-25 13:25:29 +0800

    Add new dependencies and update ESLint configuration
    - Integrated `@tanstack/react-query` and `axios` to enhance data fetching capabilities.
    - Added `@tanstack/eslint-config` and `@tanstack/eslint-plugin-query` to improve linting for React Query.
    - Updated `eslint.config.js` to include React Query plugin with recommended rules.

--- web/eslint.config.js
+++ web/eslint.config.js
@@ -1,9 +1,11 @@
 //  @ts-check
 
 import { tanstackConfig } from '@tanstack/eslint-config'
+import pluginQuery from '@tanstack/eslint-plugin-query'
 
 export default [
   ...tanstackConfig,
+  ...pluginQuery.configs['flat/recommended'],
   {
     rules: {
       'import/no-cycle': 'off',

9d19d80a9f4d27eb6d3ebbb1e73673312a7a217b
zguiyang <zhaoguiyang18@outlook.com>
2026-02-26 15:33:51 +0800

    refactor: remove unused counter store

--- web/src/stores/counter.ts
+++
@@ -1,6 +0,0 @@
-export const useCounterStore = defineStore('counter', () => {
-  const { count, inc: increment, dec: decrement, set, reset } = useCounter(0)
-  const doubleCount = computed(() => count.value * 2)
-
-  return { count, doubleCount, increment, decrement, set, reset }
-})

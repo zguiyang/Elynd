f410eeb665f16873f32d56e031b4c6a3379b253a
zguiyang <zhaoguiyang18@outlook.com>
2026-02-27 13:34:22 +0800

    feat(backend): add isAdmin property to User model

--- backend/app/models/user.ts
+++ backend/app/models/user.ts
@@ -28,6 +28,9 @@ export default class User extends compose(BaseModel, AuthFinder) {
   @column()
   declare avatar: string | null
 
+  @column()
+  declare isAdmin: boolean
+
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
 
